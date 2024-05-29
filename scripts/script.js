470013315bad1bb10438a996593f18883d552342
muhtasim-rahman <programs.turzo@gmail.com>
2024-05-29 20:58:33 +0600

    light/darlk mode function and script added  Please enter the commit message for your changes. Lines starting

--- scripts/script.js
+++ scripts/script.js
@@ -132,3 +132,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+//* Day-night mode script
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleCheckbox = document.querySelector(".toggle-checkbox");
+
+  // Check the localStorage for the current theme
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleCheckbox.checked = true;
+  }
+
+  toggleCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
